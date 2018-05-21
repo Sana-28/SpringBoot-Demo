f4383e896f6d9bbbce50f5afe4ba509de9a29149
SanaShaikh28 <SanaShaikh28>
2018-05-21 17:22:13 +0530

    First app using Spring Initializer

---
+++ SpringInitializer/src/test/java/com/springExample/SpringInitializer/SpringInitializerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springExample.SpringInitializer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringInitializerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
