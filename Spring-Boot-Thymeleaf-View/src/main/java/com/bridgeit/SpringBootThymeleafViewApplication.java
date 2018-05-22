ce02d03e75e71717bc642d3811f044aa52122737
SanaShaikh28 <SanaShaikh28>
2018-05-22 10:25:52 +0530

    Simple Html page redirect to save and display user details

---
+++ Spring-Boot-Thymeleaf-View/src/main/java/com/bridgeit/SpringBootThymeleafViewApplication.java
@@ -0,0 +1,12 @@
+package com.bridgeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafViewApplication.class, args);
+	}
+}
